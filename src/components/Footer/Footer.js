ba7eff164b14300c2a5d9f1d1a51dfc2d336a84e
Zakhar Mikhalenka <zakhar98@gmail.com>
2021-03-02 01:57:56 +0300

    Add footer and logo components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Logo from "../Logo/Logo.js";
+import "./style.scss";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <Logo />
+    </footer>
+  );
+}
